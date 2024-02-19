1c3441e9609d5d29d0cca058ed7297edfc606c94
amancooks08 <amandeepsingh.chhabra08@gmail.com>
2024-02-19 16:44:12 +0530

    feat: init new cargo project/package

---
+++ Practice/target/debug/deps/Practice-8cfb9e074f72edae.d
@@ -0,0 +1,5 @@
+/Users/amandeepsingh/Documents/Work/Rust/rust-practice/Practice/target/debug/deps/libPractice-8cfb9e074f72edae.rmeta: src/main.rs
+
+/Users/amandeepsingh/Documents/Work/Rust/rust-practice/Practice/target/debug/deps/Practice-8cfb9e074f72edae.d: src/main.rs
+
+src/main.rs:
---
+++ Practice/target/debug/deps/Practice-db6114c0421b755e.d
@@ -0,0 +1,5 @@
+/Users/amandeepsingh/Documents/Work/Rust/rust-practice/Practice/target/debug/deps/libPractice-db6114c0421b755e.rmeta: src/main.rs
+
+/Users/amandeepsingh/Documents/Work/Rust/rust-practice/Practice/target/debug/deps/Practice-db6114c0421b755e.d: src/main.rs
+
+src/main.rs:
