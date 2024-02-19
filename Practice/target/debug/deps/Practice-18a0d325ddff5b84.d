3206a5ac9686d5d4820b2ac0d04dcd0a9ea08e12
amancooks08 <amandeepsingh.chhabra08@gmail.com>
2024-02-19 18:54:14 +0530

    fix: rename the crate name to "practice" to match the snake case convention

--- Practice/target/debug/deps/Practice-18a0d325ddff5b84.d
+++ Practice/target/debug/deps/Practice-18a0d325ddff5b84.d
@@ -1,6 +1,6 @@
-/Users/amandeepsingh/Documents/Work/Rust/rust-practice/Practice/target/debug/deps/Practice-18a0d325ddff5b84: src/main.rs src/data_types.rs
+/Users/amandeepsingh/Documents/Work/Rust/rust-practice/practice/target/debug/deps/Practice-18a0d325ddff5b84: src/main.rs src/data_types.rs
 
-/Users/amandeepsingh/Documents/Work/Rust/rust-practice/Practice/target/debug/deps/Practice-18a0d325ddff5b84.d: src/main.rs src/data_types.rs
+/Users/amandeepsingh/Documents/Work/Rust/rust-practice/practice/target/debug/deps/Practice-18a0d325ddff5b84.d: src/main.rs src/data_types.rs
 
 src/main.rs:
 src/data_types.rs:
---
+++ Practice/target/debug/deps/practice-e18269e28428619e.d
@@ -0,0 +1,6 @@
+/Users/amandeepsingh/Documents/Work/Rust/rust-practice/practice/target/debug/deps/libpractice-e18269e28428619e.rmeta: src/main.rs src/data_types.rs
+
+/Users/amandeepsingh/Documents/Work/Rust/rust-practice/practice/target/debug/deps/practice-e18269e28428619e.d: src/main.rs src/data_types.rs
+
+src/main.rs:
+src/data_types.rs:
