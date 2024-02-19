9fafaa57a7f4fba2ca2a71e4b7756238c5f3b7a4
amancooks08 <amandeepsingh.chhabra08@gmail.com>
2024-02-19 16:57:05 +0530

    feat: "Hello, world!"

---
+++ Practice/target/debug/Practice.d
@@ -0,0 +1,1 @@
+/Users/amandeepsingh/Documents/Work/Rust/rust-practice/Practice/target/debug/Practice: /Users/amandeepsingh/Documents/Work/Rust/rust-practice/Practice/src/main.rs
---
+++ Practice/target/debug/deps/Practice-18a0d325ddff5b84.d
@@ -0,0 +1,5 @@
+/Users/amandeepsingh/Documents/Work/Rust/rust-practice/Practice/target/debug/deps/Practice-18a0d325ddff5b84: src/main.rs
+
+/Users/amandeepsingh/Documents/Work/Rust/rust-practice/Practice/target/debug/deps/Practice-18a0d325ddff5b84.d: src/main.rs
+
+src/main.rs:
